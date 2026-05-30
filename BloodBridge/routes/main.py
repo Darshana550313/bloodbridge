f691fe1d3291018ef694b5d1b27f3b138fbda562
Darshana550313 <166138758+Darshana550313@users.noreply.github.com>
2026-05-30 23:25:29 +0530

    Add notification read route to main blueprint

--- BloodBridge/routes/main.py
+++ BloodBridge/routes/main.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, session, url_for
 
-from models.repositories import get_inventory
+from models.repositories import get_inventory, mark_notifications_read
 
 
 main_bp = Blueprint("main", __name__)
@@ -11,3 +11,9 @@ def index():
     inventory = get_inventory()
     return render_template("index.html", inventory=inventory)
 
+
+@main_bp.route("/notifications/read", methods=["POST"])
+def read_notifications():
+    if session.get("user_id"):
+        mark_notifications_read(session["user_id"])
+    return redirect(request.referrer or url_for("main.index"))
